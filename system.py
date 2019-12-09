5d7e239f16c604204e6544d7be7ae32025f31fb1
Stetsenko Olexandr <olexstet1998@gmail.com>
2019-12-10 00:45:07 +0100

    The program is operational and reach the version 1.0. Attention: for executing the program a postgreSQL database is requested.

--- system.py
+++ system.py
@@ -1,122 +1,168 @@
-
-# Stetsenko Olexandr 
-# System code source
+# Olexandr Stetsenko
+# Last modified: 9/12/2019
 # Description:
-# This file contains python's codes for the system. 
-# Here system is initialized and created 
-# In system we have different operations that can be call by the actors 
+# Is a Small Bank system which consists to allow perform simple banking tasks
+# like send money, view personal information and add or delete an accoutn of client
+# The system is connected to a Postgresql database(local). There are two types of
+# actors Administrator and Clients. Administrator add and delete clients. The clients
+# manage their budgets by performin transactions. 
+
 import psycopg2
-from actors import * 
 
-hostname = '127.0.0.1'
-username = 'postgres'
-password = ''
-database = 'SE1-Program'
+class System:
+  def __init__(self):
+    self.hostnameD = '127.0.0.1'
+    self.usernameD = 'postgres'
+    self.passwordD = '' 
+    self.database = 'SE1-Program'
+    self.connection = psycopg2.connect("dbname ="+self.database+" user = "+self.usernameD+" password = "+self.passwordD+" host ="+self.hostnameD)
+    self.cursor = self.connection.cursor()
+    self.databaseCreated = False
+    self.nextIdentification = 10000000
 
-class system:
-  def __init__(self,name):
-    self.name = name
-    self.user = None
-    
-  def createAdmin(fisrtName,lastName,username,password):
-    cursor = databaseConnection(actor,database,username,password,localhost)
-    statement = "INSERT INTO Admin VALUES("+str(fisrtName)+","+str(lastName)+","+str(username)+","+str(password)+")"
-    cursor.execute(statement)
+  def createClientDatabase(self):
+    self.cursor.execute("DROP TABLE IF EXISTS Client;")
+    create_table_query = """CREATE TABLE Client(
+                            firstName VARCHAR,
+                            lastName VARCHAR,
+                            username VARCHAR,
+                            password VARCHAR,
+                            budget REAL,
+                            identification INTEGER,
+                            PRIMARY KEY(username,identification));"""
+    self.cursor.execute(create_table_query)
+    self.connection.commit()
 
-  def createClient(fisrtName,lastName,username,password, initialAmount):
-    cursor = databaseConnection(actor,database,username,password,localhost)
-    statement = "INSERT INTO Admin VALUES("+str(fisrtName)+","+str(lastName)+","+str(username)+","+str(password)+","+float(initialAmount)+")"
-    cursor.execute(statement)
+  def addClient(self,firstName,lastName,username,password,identification):
+    statement = "INSERT INTO Client VALUES('"+firstName+"','"+lastName+"','"+username+"','"+password+"',500,'"+identification+"')"
+    self.cursor.execute(statement)
+    self.connection.commit()
 
-  def createClientInstance(username,password,fisrtName,lastName):
-    client = actClient(fisrtName,lastName,username,password,500)
-    return client 
+  def deleteClient(self,firstName,lastName,username):
+    statement = "DELETE FROM Client WHERE Client.firstName = '"+firstName+"' and Client.lastName = '"+lastName+"' and Client.username = '"+username+"'"
+    self.cursor.execute(statement)
+    self.connection.commit()
 
-  def createAdminInstance(username,password,fisrtName,lastName):
-    client = actAdmin(fisrtName,lastName,username,password)
-    return client
+  def existClient(self,username,password):
+    statement = "SELECT * FROM Client WHERE username = '"+username+"' and password = '"+password+"'"
+    self.cursor.execute(statement)
+    found = self.cursor.fetchall()
+    if len(found) > 0:
+      return True
+    else:
+       return False
 
-  def login(username,password):
-    cursor = databaseConnection(actor,database,username,password,localhost)
-    cursor.execute("SELECT username,password,fisrtName,lastName FROM Client")
-    listClient = cursor.fetchall()
-    for user in listClient:
-      if user[0] == "username" and user[1] == "password":
-        self.user = createClientInstance(username,password,fisrtName,lastName)
-        print("You are succesfully logged. Welcome!")
-        return
+  def viewInfoClient(self,username,password):
+    statement = "SELECT * FROM Client WHERE username = '"+username+"' and password = '"+password+"'"
+    self.cursor.execute(statement)
+    infoClient = self.cursor.fetchall()
+    return infoClient
 
-    cursor.execute("SELECT username,password,fisrtName,lastName FROM Admin")
-    listAdmin = cursor.fetchall()
-    for user in listAdmin:
-      if user[0] == "username" and user[1] == "password":
-        self.user = createAdminInstance(username,password,fisrtName,lastName)
-        print("You are succesfully logged. Welcome!")
-        return
+  def sendMoney(self,usernameSource,amount,identification):
+    if amount <= 0:
+      print("Invalid amount")
+      return
+    statement = "SELECT * FROM Client WHERE identification = "+identification
+    self.cursor.execute(statement)
+    infoDestClient = self.cursor.fetchall()
+    if len(infoDestClient) > 0:
+      infoDestClient = infoDestClient[0]
+      statement = "SELECT * FROM Client WHERE username = '"+usernameSource+"'"
+      self.cursor.execute(statement)
+      infoSourceClient = self.cursor.fetchall()[0]
+      if float(infoSourceClient[4]) < float(amount):
+        print("Transaction is not possible, not enough money\n")
+      else:
 
-  def logout():
-    self.user = None
-    print("Succesfully exit the system. See you next time!")
-    return 
-
-  def oeSentMoney(firstName, lastName,amount):
-    cursor = databaseConnection(actor,database,username,password,localhost)
-    usernameSender = self.user.username
-    cursor.execute("SELECT username,amount FROM Client WHERE firstName ="+str(firstName)+", lastName ="+str(lastName))
-    result = cursor.fetchall()
-    usernameReceiver,amountReceiver = result[0], result[1]
-    amountReceiver += amount
-    statement = "SELECT amount"
-    cursor.execute()
-             	
-  def databaseConnection(actor,database,username,password,localhost):
-    connection = psycopg2.connect("dbname ="+database+" user = "+username+" password = "+password+" host = "+localhost)
-    cursor = connection.cursor()
-    return cursor
-
-  def createDatabase(actor,database,username,password,localhost):
-    cursor = databaseConnection(actor,database,username,password,localhost)
-    if(type(actor).__name__ == "actClient"):
-      cursor.execute("DROP TABLE IF EXISTS Client;")
-
-      create_table_query = '''CREATE TABLE Client(
-          					FirstName VARCHAR(255),
-          					LastName VARCHAR(255),
-          					Username VARCHAR(255),
-          					Password VARCHAR(255),
-          					Amount REAL,
-          					PRIMARY KEY(Username)
-                                              );'''
-      cursor.execute(create_table_query)
-
-    if type(actor).__name__ == "actAdmin":
-      cursor.execute("DROP TABLE IF EXISTS Admin;")
-      create_table_query = '''CREATE TABLE Admin(
-          					FirstName VARCHAR(255),
-          					LastName VARCHAR(255),
-          					Username VARCHAR(255),
-          					Password VARCHAR(255),
-          					PRIMARY KEY(Username)
-                                                      );'''
-      cursor.execute(create_table_query)
-                 
+        statement = "UPDATE Client SET budget = "+str(float(infoSourceClient[4])-float(amount))+" WHERE client.username = '"+usernameSource+"'"
+        self.cursor.execute(statement)
+        statement = "UPDATE Client SET budget = "+str(float(infoSourceClient[4])+float(amount))+" WHERE client.identification = '"+identification+"'"
+        self.cursor.execute(statement)
+        self.connection.commit()
+        print("Transaction success!\n")
+    else:
+      print("invalid identification!")
 
 
 
 def main():
-  admin = createAdmin("Olexandr","Stetsenko","olex","stet")
-  
-  
+  print("Hello")
+  system = System()
+  system.createClientDatabase()
+  system.databaseCreated = True
+  while True: 
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
 
-		
+    if username == "system" and password =="system":
+      cmd = input("Close Connection: ")
+      if cmd.lower() == "yes":
+        system.connection.close()
+        print("Sysytem destroyed!")
+        break
 
+    elif username == "admin" and password == "admin":
+      print("Welcome Administrator")
+      while True:
+        print("Your commands are as follow: addNewClient, deleteClient, logout")
+        cmd = input("Please enter a command: ")
+        
+        if cmd == "addNewClient" and system.databaseCreated == True:
+          print("Create new client:")
+          firstNameC = input("Client first name: ")
+          lastNameC = input("Client last name: ")
+          usernameC = input("Client username: ")
+          passwordC = input("Client password: ")
+          system.addClient(firstNameC,lastNameC,usernameC,passwordC,str(system.nextIdentification))
+          system.nextIdentification = system.nextIdentification + 1 
+          print("Client is added to database!\n")
 
+        elif cmd == "deleteClient" and system.databaseCreated == True:
+          print("Create enter client information:")
+          firstNameC = input("Client first name: ")
+          lastNameC = input("Client last name: ")
+          usernameC = input("Client username: ")
+          system.deleteClient(firstNameC,lastNameC,usernameC)
+          print("Client is deleted from database!\n")
 
+        elif cmd == "logout":
+          print("You are logout!\n")
+          break
 
+        else:
+          print("Invalid Command!")
 
+    elif system.existClient(username,password):
+      infoClient = system.viewInfoClient(username,password)
+      clientFirstName = infoClient[0][0]
+      clientLastName = infoClient[0][1]
+      print("Welcome",clientFirstName,clientLastName,"\n")
+      while True:
+        print("For doing a transaction, use command: sendMoney")
+        print("If you want logout, use command: logout")
+        print("For viewing the information of your account, use command: viewInfo\n")
+        cmd = input("Please enter a command: ")
+        if cmd == "sendMoney":
+          amount = input("amount: ")
+          identification = input("To identification: ")
+          system.sendMoney(username,int(amount),identification)
 
+        elif cmd == "viewInfo":
+          infoClient = system.viewInfoClient(username,password)
+          print("First Name:",infoClient[0][0])
+          print("Last Name:",infoClient[0][1])
+          print("Username:",infoClient[0][2])
+          print("Password:",infoClient[0][3])
+          print("Budget:",infoClient[0][4])
+          print("Identification:",infoClient[0][5],"\n")
 
+        elif cmd == "logout":
+          print("You are logout!\n")
+          break
 
+        else:
+          print("Invalid Command!")
 
-
-
+    else:
+      print("Invalid Username or password!\n")
+main()
